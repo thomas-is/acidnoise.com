45fa0f76556935886b30260925d2b909eb244ee5
thomas-is <thomas@0l.fi>
2022-06-20 19:55:37 +0200

    :whale: update nginx config bind mount

--- run-local.sh
+++ run-local.sh
@@ -2,5 +2,5 @@
 
 docker run --rm -d --name acidnoise \
   -v $(pwd)/srv:/srv \
-  -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf \
+  -v $(pwd)/nginx.conf:/etc/nginx/http.d/default.conf \
   -p 8080:80 0lfi/ng-php7
