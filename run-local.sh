d8b042bc85ba6f3536fd7c15600fc5609e20c139
thomas-is <thomas@0l.fi>
2023-11-08 15:30:09 +0100

    🐚 display local URL

--- run-local.sh
+++ run-local.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+port=8080
+echo "http://localhost:$port"
 docker run --rm -it --name acidnoise \
   -v $(pwd)/srv:/srv \
   -v $(pwd)/nginx.conf:/etc/nginx/http.d/default.conf \
-  -p 8080:80 0lfi/ng-php7
+  -p $port:80 0lfi/ng-php7
